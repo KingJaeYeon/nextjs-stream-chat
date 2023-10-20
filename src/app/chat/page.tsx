eaa2a351c5337d78e1b53cea5589f9411de52e5f
pjy <wodus331@gmail.com>
2023-10-20 14:58:59 +0900

    chapter 2 중간세이브

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,46 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { StreamChat } from "stream-chat";
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+const userId = "user_2X0U1dDmMRvMnjFCB9SEXgb1Q0n";
+
+const chatClient = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY!);
+
+chatClient.connectUser(
+  {
+    id: userId,
+    name: "재연 박",
+  },
+  //process.env.STREAM_SECRET  + userId => this is the token
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yWDBVMWREbU1Sdk1uakZDQjlTRVhnYjFRMG4ifQ.psuC_A9LY0xZIwmudovPBOM6HrH3XOZqNPBCBrFmJ80"
+);
+
+const channel = chatClient.channel("messaging", "channel_1", {
+  name: "Channel #1",
+  members: [userId],
+});
 
 export default function ChatPage() {
   return (
     <div>
-      this is chat
-      <UserButton afterSignOutUrl="/" />
+      <Chat client={chatClient}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
 }
