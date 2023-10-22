f354103486ca652b0e56eb71776fee94b24cf79a
pjy <wodus331@gmail.com>
2023-10-22 15:53:06 +0900

    chapter 7 useDebounce 중요

--- src/app/chat/UserMenu.tsx
+++ src/app/chat/UserMenu.tsx
@@ -2,12 +2,13 @@ import {
   Avatar,
   useChatContext,
   LoadingChannels as LoadingUsers,
-} from "stream-chat-react";
-import { useEffect, useState } from "react";
-import { UserResource } from "@clerk/types";
-import { Channel, UserResponse } from "stream-chat";
-import { ArrowLeft } from "lucide-react";
-import LoadingButton from "@/components/LoadingButton";
+} from 'stream-chat-react';
+import { useEffect, useState } from 'react';
+import { UserResource } from '@clerk/types';
+import { Channel, UserResponse } from 'stream-chat';
+import { ArrowLeft } from 'lucide-react';
+import LoadingButton from '@/components/LoadingButton';
+import useDebounce from '@/hooks/useDebounce';
 
 interface UsersMenuProps {
   loggedInUser: UserResource;
@@ -22,21 +23,36 @@ export default function UserMenu({
 }: UsersMenuProps) {
   const { client, setActiveChannel } = useChatContext();
 
-  const [users, setUsers] = useState<UserResponse[]>();
+  const [users, setUsers] = useState<(UserResponse & { image?: string })[]>();
+
+  const [searchInput, setSearchInput] = useState('');
+  const searchInputDebounced = useDebounce(searchInput);
 
   const [moreUsersLoading, setMoreUsersLoading] = useState(false);
 
   const [endOfPaginationReached, setEndOfPaginationReached] =
-    useState<boolean>(false);
+    useState<boolean>();
 
   const pageSize = 10;
 
   useEffect(() => {
     async function loadInitialUsers() {
+      setUsers(undefined);
+      setEndOfPaginationReached(undefined);
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
+
       try {
         const response = await client.queryUsers(
           {
             id: { $ne: loggedInUser.id },
+            ...(searchInputDebounced
+              ? {
+                  $or: [
+                    { name: { $autocomplete: searchInputDebounced } },
+                    { id: { $autocomplete: searchInputDebounced } },
+                  ],
+                }
+              : {}),
           },
           { id: 1 },
           { limit: pageSize + 1 }
@@ -45,11 +61,11 @@ export default function UserMenu({
         setEndOfPaginationReached(response.users.length <= pageSize);
       } catch (e) {
         console.log(e);
-        alert("Error loading users ");
+        alert('Error loading users ');
       }
     }
     loadInitialUsers();
-  }, [client, loggedInUser.id]);
+  }, [client, loggedInUser.id, searchInputDebounced]);
 
   async function loadMoreUsers() {
     setMoreUsersLoading(true);
@@ -62,7 +78,18 @@ export default function UserMenu({
 
       const response = await client.queryUsers(
         {
-          $and: [{ id: { $ne: loggedInUser.id } }, { id: { $gt: lastUserId } }],
+          $and: [
+            { id: { $ne: loggedInUser.id } },
+            { id: { $gt: lastUserId } },
+            searchInputDebounced
+              ? {
+                  $or: [
+                    { name: { $autocomplete: searchInputDebounced } },
+                    { id: { $autocomplete: searchInputDebounced } },
+                  ],
+                }
+              : {},
+          ],
         },
         { id: 1 },
         { limit: pageSize + 1 }
@@ -71,7 +98,7 @@ export default function UserMenu({
       setEndOfPaginationReached(response.users.length <= pageSize);
     } catch (error) {
       console.log(error);
-      alert("Error loading users ");
+      alert('Error loading users ');
     } finally {
       setMoreUsersLoading(false);
     }
@@ -84,24 +111,32 @@ export default function UserMenu({
 
   async function startChatWithUser(userId: string) {
     try {
-      const channel = client.channel("messaging", {
+      const channel = client.channel('messaging', {
         members: [userId, loggedInUser.id],
       });
       await channel.create();
       handleChannelSelected(channel);
     } catch (e) {
       console.log(e);
-      alert("Error creating channel");
+      alert('Error creating channel');
     }
   }
 
   return (
     <div className="str-chat absolute z-10 h-full w-full border-e border-e-[#DBDDE1] bg-white">
-      <div className="flex items-center gap-3 p-3 text-lg font-bold">
-        <ArrowLeft onClick={onClose} className="cursor-pointer" />
+      <div className="flex flex-col p-3">
+        <div className="mb-3 flex items-center gap-3 text-lg font-bold">
+          <ArrowLeft onClick={onClose} className="cursor-pointer" />
+        </div>
+        <input
+          type="search"
+          placeholder="Search"
+          className="rounded-full border border-gray-300 px-4 py-2"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
       </div>
       <div>
-        {!users && <LoadingUsers />}
         {users?.map((user) => (
           <UserResult
             user={user}
@@ -109,6 +144,11 @@ export default function UserMenu({
             key={user.id}
           />
         ))}
+        <div className="px-3">
+          {!users && !searchInputDebounced && <LoadingUsers />}
+          {!users && searchInputDebounced && 'Searching...'}
+          {users?.length === 0 && <div>No users found</div>}
+        </div>
         {!endOfPaginationReached && (
           <LoadingButton
             onClick={loadMoreUsers}
