b5acc054cfdbf139822488ed65d69f78b74c43fa
pjy <wodus331@gmail.com>
2023-10-22 18:09:49 +0900

    chapter 8

--- src/app/chat/UserMenu.tsx
+++ src/app/chat/UserMenu.tsx
@@ -2,13 +2,14 @@ import {
   Avatar,
   useChatContext,
   LoadingChannels as LoadingUsers,
-} from 'stream-chat-react';
-import { useEffect, useState } from 'react';
-import { UserResource } from '@clerk/types';
-import { Channel, UserResponse } from 'stream-chat';
-import { ArrowLeft } from 'lucide-react';
-import LoadingButton from '@/components/LoadingButton';
-import useDebounce from '@/hooks/useDebounce';
+} from "stream-chat-react";
+import { useEffect, useState } from "react";
+import { UserResource } from "@clerk/types";
+import { Channel, UserResponse } from "stream-chat";
+import { ArrowLeft } from "lucide-react";
+import LoadingButton from "@/components/LoadingButton";
+import useDebounce from "@/hooks/useDebounce";
+import Button from "@/components/Button";
 
 interface UsersMenuProps {
   loggedInUser: UserResource;
@@ -25,7 +26,9 @@ export default function UserMenu({
 
   const [users, setUsers] = useState<(UserResponse & { image?: string })[]>();
 
-  const [searchInput, setSearchInput] = useState('');
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+
+  const [searchInput, setSearchInput] = useState("");
   const searchInputDebounced = useDebounce(searchInput);
 
   const [moreUsersLoading, setMoreUsersLoading] = useState(false);
@@ -61,7 +64,7 @@ export default function UserMenu({
         setEndOfPaginationReached(response.users.length <= pageSize);
       } catch (e) {
         console.log(e);
-        alert('Error loading users ');
+        alert("Error loading users ");
       }
     }
     loadInitialUsers();
@@ -98,7 +101,7 @@ export default function UserMenu({
       setEndOfPaginationReached(response.users.length <= pageSize);
     } catch (error) {
       console.log(error);
-      alert('Error loading users ');
+      alert("Error loading users ");
     } finally {
       setMoreUsersLoading(false);
     }
@@ -111,19 +114,32 @@ export default function UserMenu({
 
   async function startChatWithUser(userId: string) {
     try {
-      const channel = client.channel('messaging', {
+      const channel = client.channel("messaging", {
         members: [userId, loggedInUser.id],
       });
       await channel.create();
       handleChannelSelected(channel);
     } catch (e) {
       console.log(e);
-      alert('Error creating channel');
+      alert("Error creating channel");
     }
   }
 
+  async function startGroupChat(members?: string[], name?: string) {
+    try {
+      const channel = client.channel("messaging", {
+        members,
+        name,
+      });
+      await channel.create();
+      handleChannelSelected(channel);
+    } catch (e) {
+      console.log(e);
+      alert("Error creating channel");
+    }
+  }
   return (
-    <div className="str-chat absolute z-10 h-full w-full border-e border-e-[#DBDDE1] bg-white">
+    <div className="str-chat absolute z-10 h-full w-full overflow-y-auto border-e border-e-[#DBDDE1] bg-white">
       <div className="flex flex-col p-3">
         <div className="mb-3 flex items-center gap-3 text-lg font-bold">
           <ArrowLeft onClick={onClose} className="cursor-pointer" />
@@ -136,17 +152,33 @@ export default function UserMenu({
           onChange={(e) => setSearchInput(e.target.value)}
         />
       </div>
+      {selectedUsers.length > 0 && (
+        <StartGroupChatHeader
+          onConfirm={(name) =>
+            startGroupChat([loggedInUser.id, ...selectedUsers], name)
+          }
+          onClearSelection={() => setSelectedUsers([])}
+        />
+      )}
       <div>
         {users?.map((user) => (
           <UserResult
             user={user}
+            selected={selectedUsers.includes(user.id)}
+            onChangeSelected={(selected) =>
+              setSelectedUsers(
+                selected
+                  ? [...selectedUsers, user.id]
+                  : selectedUsers.filter((x) => x !== user.id)
+              )
+            }
             onUserClick={startChatWithUser}
             key={user.id}
           />
         ))}
         <div className="px-3">
           {!users && !searchInputDebounced && <LoadingUsers />}
-          {!users && searchInputDebounced && 'Searching...'}
+          {!users && searchInputDebounced && "Searching..."}
           {users?.length === 0 && <div>No users found</div>}
         </div>
         {!endOfPaginationReached && (
@@ -166,14 +198,28 @@ export default function UserMenu({
 interface UserResultProps {
   user: UserResponse & { image?: string };
   onUserClick: (userId: string) => void;
+  selected?: boolean;
+  onChangeSelected?: (selected: boolean) => void;
 }
 
-function UserResult({ user, onUserClick }: UserResultProps) {
+function UserResult({
+  user,
+  onUserClick,
+  selected,
+  onChangeSelected,
+}: UserResultProps) {
   return (
     <button
       className="mb-3 flex w-full items-center gap-2 p-2 hover:bg-[#e9eaed]"
       onClick={() => onUserClick(user.id)}
     >
+      <input
+        type="checkbox"
+        className="mx-1 scale-125"
+        checked={selected}
+        onChange={(e) => onChangeSelected?.(e.target.checked)}
+        onClick={(e) => e.stopPropagation()}
+      />
       <span>
         <Avatar image={user.image} name={user.name || user.id} size={40} />
       </span>
@@ -182,5 +228,38 @@ function UserResult({ user, onUserClick }: UserResultProps) {
       </span>
       {user.online && <span className="text-xs text-green-500">Online</span>}
     </button>
+  );
+}
+
+interface StartGroupChatHeaderProps {
+  onConfirm: (name?: string) => void;
+  onClearSelection: () => void;
+}
+
+function StartGroupChatHeader({
+  onConfirm,
+  onClearSelection,
+}: StartGroupChatHeaderProps) {
+  const [groupChatNameInput, setGroupChatNameInput] = useState("");
+  return (
+    <div className="sticky top-0 z-10 flex flex-col gap-3 bg-white p-3 shadow-sm">
+      <input
+        placeholder="Group name"
+        className="rounded border border-gray-300 py-2"
+        value={groupChatNameInput}
+        onChange={(e) => setGroupChatNameInput(e.target.value)}
+      />
+      <div className="flex justify-center gap-2">
+        <Button className="py-2" onClick={() => onConfirm(groupChatNameInput)}>
+          Start group chat
+        </Button>
+        <Button
+          onClick={onClearSelection}
+          className="bg-gray-400 py-2 active:bg-gray-500"
+        >
+          Clear selection
+        </Button>
+      </div>
+    </div>
   );
 }
